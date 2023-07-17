d7e3b9f48fbd3200facf107d27e69a09e196f27d
CesarOrtiz1994 <cesarortize94@gmail.com>
2023-07-17 17:50:54 -0600

    inicio del proyecto

---
+++ src/main/java/com/example/subirimagen/SubirImagenApplication.java
@@ -0,0 +1,13 @@
+package com.example.subirimagen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubirImagenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubirImagenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/subirimagen/SubirImagenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subirimagen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubirImagenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
